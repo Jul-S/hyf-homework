5656aed0d7d612b64033893203bee2fe448b0b1f
jul-s <jul.samorodova@gmail.com>
2021-05-04 22:46:01 +0200

    Adding Ez Namey (Startup name generator) solution

---
+++ javascript/javascript1/week1/ez_namey.js
@@ -0,0 +1,10 @@
+const firstWords = ["Easy", "Awesome", "Corporate", "Severe", "Smiling", 
+    "Heavenly", "Childlike", "Shy", "Anxious", "Ugliest", "Accurate"];
+const secondWords = ["World", "Ladder", "Exam", "Driver", "Organization",
+    "Operation", "Sector", "Oven", "Atmosphere", "Product"];
+
+const randomNumber = Math.floor(Math.random() * 10) + 0;
+
+let startupName = firstWords[randomNumber] + " " + secondWords[randomNumber];
+
+console.log(`The startup: "${startupName}" contains ${startupName.length} characters`)
