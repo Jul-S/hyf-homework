e692639878d8eea5c45b97dd4ea5651299da8943
jul-s <jul.samorodova@gmail.com>
2021-05-05 19:53:48 +0200

    Adding conditions to pizza.js and goodboy-oldboy.js

--- javascript/javascript1/week1/goodboy-oldboy.js
+++ javascript/javascript1/week1/goodboy-oldboy.js
@@ -3,11 +3,12 @@ const dogYearFuture = 2027;
 
 let dogYear = dogYearFuture - dogYearOfBirth;
 
-// 1st case showing in human years
 let shouldShowResultInDogYears = false;
-console.log(`Your dog will be ${dogYear} human years old in ${dogYearFuture}`);
 
-// 2nd case showing in dog years
-shouldShowResultInDogYears = true;
-dogYear *= 7;
-console.log(`Your dog will be ${dogYear} human years old in ${dogYearFuture}`);
+if (shouldShowResultInDogYears) {
+    dogYear *= 7;
+    console.log(`Your dog will be ${dogYear} dog years old in ${dogYearFuture}`);
+} else {
+    console.log(`Your dog will be ${dogYear} human years old in ${dogYearFuture}`);
+}
+
