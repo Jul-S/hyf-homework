54370fe1bd923cd2da8d92ec27b340f54d60b93d
jul-s <jul.samorodova@gmail.com>
2021-05-05 19:41:35 +0200

    Adding random number depending on array length

--- javascript/javascript1/week1/ez_namey.js
+++ javascript/javascript1/week1/ez_namey.js
@@ -1,10 +1,12 @@
-const firstWords = ["Easy", "Awesome", "Corporate", "Severe", "Smiling", 
+const firstWords = ["Easy", "Awesome", "Corporate", "Severe", "Smiling",
     "Heavenly", "Childlike", "Shy", "Anxious", "Ugliest", "Accurate"];
 const secondWords = ["World", "Ladder", "Exam", "Driver", "Organization",
     "Operation", "Sector", "Oven", "Atmosphere", "Product"];
 
-const randomNumber = Math.floor(Math.random() * 10) + 0;
+//making the random number generator depend on the length of longer array
+const firstRandomNumber = Math.floor(Math.random() * firstWords.length);
+const secondRandomNumber = Math.floor(Math.random() * secondWords.length);
 
-let startupName = firstWords[randomNumber] + " " + secondWords[randomNumber];
+let startupName = firstWords[firstRandomNumber] + " " + secondWords[secondRandomNumber];
 
 console.log(`The startup: "${startupName}" contains ${startupName.length} characters`);
