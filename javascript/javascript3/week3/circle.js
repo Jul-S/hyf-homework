753514db9fc9668cd08ac91005ca36569cbad803
jul-s <jul.samorodova@gmail.com>
2021-07-09 18:10:19 +0200

    Circles apear randomly finished

--- javascript/javascript3/week3/circle.js
+++ javascript/javascript3/week3/circle.js
@@ -15,6 +15,7 @@ class Circle {
         context.beginPath();
         context.arc(this.x, this.y, this.radius, this.startAngle, this.endAngle);
         context.fillStyle = this.fillColor;
+        context.lineWidth = 1;
         context.fill();
         context.stroke();
     }
@@ -23,9 +24,9 @@ class Circle {
 setInterval(renderCircles, 100);
 
 function renderCircles() {
-    const randomX = Math.floor(Math.random() * canvas.clientWidth);
-    const randomY = Math.floor(Math.random() * canvas.clientHeight);
-    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    const c1 = new Circle(50, 50, 20, 0, 2 * Math.PI, "#000000");
+    const randomX = Math.floor(Math.random() * canvas.width);
+    const randomY = Math.floor(Math.random() * canvas.height);
+    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    const c1 = new Circle(randomX, randomY, 20, 0, 2 * Math.PI, randomColor);
     c1.draw();
 }
