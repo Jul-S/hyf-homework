9314e6df711b67b36629e8a5dd0ce2a608191122
jul-s <jul.samorodova@gmail.com>
2021-07-10 17:30:35 +0200

    Added optional feature with autocompte and currency fetch

--- javascript/javascript3/week3/shop-cart/main.js
+++ javascript/javascript3/week3/shop-cart/main.js
@@ -1,25 +1,42 @@
 class Product {
     constructor(name, price) {
-        this.id = this.generateUUID();
+        this.id;
+        this.generateUUID();
         this.name = name;
         this.price = price;
         this.currency = "DKK";
     }
-    convertToCurrency(currency) {
-        fetchCurrencyRate()
-        this.currency = currency;
+
+    convertToCurrency(exchangeCurrency) {
+        const base = this.currency.toLowerCase();
+        const exchange = exchangeCurrency.toLowerCase();
+        this.fetchRate(base, exchange);
+        this.currency = exchangeCurrency;
     }
+
+    async fetchRate(base, exchange) {
+        let rate;
+        await fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${base}.json`)
+            .then(res => res.json())
+            .then(data => {
+                rate = data[base][exchange];
+                this.price = this.price * rate;
+            });
+    }
+
     //adding my feature: generate UUID using api
     async generateUUID() {
         await fetch("https://www.uuidtools.com/api/generate/v1/count/1")
             .then(res => res.json())
             .then(data => this.id = data[0]);
     }
+
+    getPriceString() {
+        return this.price.toLocaleString('en-EN', { style: 'currency', currency: this.currency, minimumFractionDigits: 2 })
+    }
+
 }
 
-function fetchCurrencyRate() {
-
-}
 
 class ShoppingCart {
     constructor() {
@@ -50,11 +67,11 @@ class ShoppingCart {
 
     renderProducts() {
         const productList = document.createElement("ul");
+        productList.setAttribute("id", "products-list")
         this.products.forEach(product => {
             const prodListItem = document.createElement("li");
             const productTitle = document.createElement("p");
-            const priceString = product.price.toLocaleString('en-EN', { style: 'currency', currency: product.currency, minimumFractionDigits: 2 })
-            productTitle.innerHTML = `${product.name}: ${priceString} ${product.currency}`
+            productTitle.innerHTML = `${product.name}: ${product.getPriceString()}`
             prodListItem.appendChild(productTitle);
             productList.appendChild(prodListItem);
         })
@@ -68,20 +85,23 @@ class ShoppingCart {
     }
 }
 
+//testing cart
 const shoppingCart = new ShoppingCart();
 
 const products = [
     new Product("flat-screen", 5000),
+    new Product("phototocamera", 13000),
     new Product("phone", 300),
     new Product("laptop", 3500),
     new Product("x-box", 4600)];
 
 //testing change currency featutre
 products.forEach(p => p.convertToCurrency("USD"));
+console.log(products);
 
 products.forEach(p => shoppingCart.addProduct(p));
 
-//render cart after fetched User
+//render cart after User fetched
 shoppingCart.getUser().then(data => {
     renderShoppingCart(data);
 });
@@ -99,10 +119,77 @@ function renderShoppingCart(userData) {
 
     shoppingCart.renderProducts();
 
-
     const totalPrice = document.createElement("h2");
     totalPrice.innerHTML = `Total: ${shoppingCart.getTotal()}`;
     document.body.appendChild(totalPrice);
 
-    addSearchProductsInput();
+    addSearchProductsWithAutocomplete();
+}
+
+//adding optional features:
+//1. search with autocomplete
+//2. open product modal when autocomplete clicked
+function addSearchProductsWithAutocomplete() {
+    const searchDiv = document.createElement("div");
+    searchDiv.setAttribute("class", "autocomplete");
+    const searchInput = document.createElement("input");
+    searchInput.setAttribute("type", "text");
+    searchInput.setAttribute("placeholder", "Search product");
+    searchDiv.appendChild(searchInput);
+    document.body.insertBefore(searchDiv, document.getElementById("products-list"))
+
+    addAutocomplete(searchInput)
+}
+
+function addAutocomplete(searchInput) {
+
+    searchInput.addEventListener("input", function () {
+        const inpValue = this.value;
+        closeAutocomplete();
+        if (!inpValue) return false;
+
+        const autocompleteDiv = document.createElement("div");
+        autocompleteDiv.setAttribute("class", "autocomplete-items");
+        this.parentNode.appendChild(autocompleteDiv);
+
+        shoppingCart.products.forEach(product => {
+            if (product.name.toUpperCase().startsWith(inpValue.toUpperCase())) {
+                const resultDiv = document.createElement("div");
+                /*make the matching letters bold:*/
+                resultDiv.innerHTML = "<strong>" + product.name.slice(0, inpValue.length) + "</strong>";
+                resultDiv.innerHTML += product.name.slice(inpValue.length);
+                // adding listener to manage click
+                resultDiv.addEventListener("click", function () {
+                    closeAutocomplete();
+                    openProductModal(product);
+                });
+
+                autocompleteDiv.appendChild(resultDiv);
+            }
+        });
+
+    });
+
+    function openProductModal(product) {
+        let productModal = document.querySelector("dialog");
+        if (!productModal) {
+            productModal = document.createElement("dialog");
+            document.body.appendChild(productModal);
+        }
+        productModal.innerHTML = `<p>${product.name} - ${product.getPriceString()}</p>`
+        productModal.showModal();
+        //add listener to close modal
+        productModal.addEventListener("click", function () { this.close() });
+    }
+
+    function closeAutocomplete() {
+        const items = document.getElementsByClassName("autocomplete-items");
+        for (var i = 0; i < items.length; i++) {
+            items[i].parentNode.removeChild(items[i]);
+        }
+    }
+    //close all autocomplete lists when someone clicks in the document
+    document.addEventListener("click", function (e) {
+        closeAutocomplete();
+    });
 }
